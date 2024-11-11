20055074e8d8693bba105c3a00a1f3d106461443
abbasi303 <usalcgg18@gmail.com>
2024-11-11 03:12:10 +0100

    yolo utput

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 model= YOLO('yolov8x')
 
-results=model.predict('input_video/08fd33_4.mp4',save=True)
+results = model.predict('input_video/08fd33_4.mp4', save=True, project='runs/detect', name='output_video')
 print(results[0])
 print('=============================')
 for box in results[0].boxes:
